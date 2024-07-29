309b23a99d76342b1a6933ae98099782f3d0a5fc
kirillrmnvch <kirillpolesuk05@gmail.com>
2024-07-29 15:13:58 +0500

    change png web-application

--- full_web_application_bot/main.py
+++ full_web_application_bot/main.py
@@ -8,7 +8,7 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start'])
 async def start(message: types.message):
     markup = types.ReplyKeyboardMarkup()
-    markup.add(types.KeyboardButton('Открыть веб страницу', web_app=WebhookInfo(url='https://rawcdn.githack.com/PoleschukKR/telegram_bots_practice/main/full_web_application_bot/index.html')))
+    markup.add(types.KeyboardButton('Открыть веб страницу', web_app=WebhookInfo(url='https://raw.githack.com/PoleschukKR/telegram_bots_practice/06def83435dcaa37bc7fe54af6357158b3c09832/full_web_application_bot/index.html')))
     await message.answer('Привет!', reply_markup=markup)
 
 executor.start_polling(dp)
